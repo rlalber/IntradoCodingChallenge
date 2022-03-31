eedfdf3cfa1be193dddc8fd53155f355b0cc3efe
Russ L ALbert <17299608+rlalber@users.noreply.github.com>
2022-03-31 13:01:43 -0600

    Final changes
    Final changes

---
+++ src/main/java/intrado/schoolmessenger/coding/challenge/emailservice/EmailServiceConfig.java
@@ -0,0 +1,10 @@
+package intrado.schoolmessenger.coding.challenge.emailservice;
+
+public class EmailServiceConfig
+{
+    // gmail config info is in resources/application.properties
+    public static final String ARCHIVE_DIR = "archive";
+    public static final String ARCHIVE_FILE = "EmailMessage";
+    public static final String ARCHIVE_TYPE = "json";
+    public static final String FROM = "rlalber1@gmail.com";
+}
